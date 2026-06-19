c3db38699ccd65016cb8c72ae6a87a663acb6b8c
Emmanuel Obe <113615572+emobe6422@users.noreply.github.com>
2026-06-19 10:08:35 -0700

    bugs present
    Currently the pixel moves incredibly fast and the border controls to prevent the pixel from looping around don't work correctly.

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdint.h>
+
+//bit masks
+#define BTN_RIGHT 0x00000001
+#define BTN_FIRE  0x00000002
+#define BTN_LEFT  0x00000004
+
+int main () {
+
+    volatile uint32_t* btn_reg  = (volatile uint32_t*)0x10000000;
+    volatile uint32_t* px_reg = (volatile uint32_t*)0x20000000;
+    volatile uint32_t* py_reg = (volatile uint32_t*)0x30000000;
+
+    uint32_t player_x = 320;
+    const uint32_t player_y = 400;
+    
+    *px_reg = player_x;
+    *py_reg = player_y;
+    //the vlaues in button_reg are received from the physical button
+    while (1) {
+        //bit masks used so when we add shooting, we can move and shoot at the same time
+          //If we added == instead of &, that would evaluate the entire value
+        if (*btn_reg & BTN_LEFT) {
+            //move left 1 pixel
+            if (player_x >= 0) {
+                *px_reg = --player_x;
+            }
+        } 
+        if (*btn_reg & BTN_RIGHT) {
+            //move right 1 pixel
+            if (player_x < 639) {
+                *px_reg = ++player_x;
+                //After we surpass 639, we can no longer move
+            } else if (player_x == 639) {
+                *px_reg = 639;
+            }
+        }
+    }
+    return (0);
+}
