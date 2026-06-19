74ab8ff6be5e21651d8d45554c7ea353a09e767b
Emmanuel Obe <113615572+emobe6422@users.noreply.github.com>
2026-06-19 15:11:40 -0400

    Update main.c
    Pixel is moving well now. Going to try making it into a sprite next and figuring out ROM stuff in rtl

--- main.c
+++ main.c
@@ -7,12 +7,15 @@
 
 int main () {
 
-    volatile uint32_t* btn_reg  = (volatile uint32_t*)0x10000000;
+    volatile uint32_t* btn_reg = (volatile uint32_t*)0x10000000;
     volatile uint32_t* px_reg = (volatile uint32_t*)0x20000000;
     volatile uint32_t* py_reg = (volatile uint32_t*)0x30000000;
 
     uint32_t player_x = 320;
-    const uint32_t player_y = 400;
+    const uint32_t player_y = 430;
+    //change to byte later using typedef
+    uint32_t counter = 0; //
+    const uint32_t division_rate = 1000000; // one million
     
     *px_reg = player_x;
     *py_reg = player_y;
@@ -20,20 +23,23 @@ int main () {
     while (1) {
         //bit masks used so when we add shooting, we can move and shoot at the same time
           //If we added == instead of &, that would evaluate the entire value
-        if (*btn_reg & BTN_LEFT) {
-            //move left 1 pixel
-            if (player_x >= 0) {
-                *px_reg = --player_x;
+        if (counter >= division_rate) {
+            if (*btn_reg & BTN_LEFT) {
+                //move left 1 pixel
+                //if (player_x >= 0) {
+                    *px_reg = --player_x;
+                //}
+            } 
+            if (*btn_reg & BTN_RIGHT) {
+                //move right 1 pixel
+                //if (player_x <= 639) { temporary removal
+                    *px_reg = ++player_x;
+                    //After we surpass 639, we can no longer move
+                //}
             }
-        } 
-        if (*btn_reg & BTN_RIGHT) {
-            //move right 1 pixel
-            if (player_x < 639) {
-                *px_reg = ++player_x;
-                //After we surpass 639, we can no longer move
-            } else if (player_x == 639) {
-                *px_reg = 639;
-            }
+            counter = 0;
+        } else {
+            counter++;
         }
     }
     return (0);
