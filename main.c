ba72588cbec85e23088b5155ebb28e7e3bf90bfe
Emmanuel Obe <113615572+emobe6422@users.noreply.github.com>
2026-06-26 18:24:35 -0400

    Player logic completed (no collision logic yet)

--- main.c
+++ main.c
@@ -6,41 +6,71 @@
 #define BTN_LEFT  0x00000004
 
 int main () {
-
+    //buttons: left, right, fire
     volatile uint32_t* btn_reg = (volatile uint32_t*)0x10000000;
+    //player
     volatile uint32_t* px_reg = (volatile uint32_t*)0x20000000;
     volatile uint32_t* py_reg = (volatile uint32_t*)0x30000000;
+    //bullet
+    volatile uint32_t* bullet_px_reg = (volatile uint32_t*)0x40000000;
+    volatile uint32_t* bullet_py_reg = (volatile uint32_t*)0x50000000;
 
+    //initial player coordinates
     uint32_t player_x = 320;
     const uint32_t player_y = 430;
-    //change to byte later using typedef
-    uint32_t counter = 0; //
-    const uint32_t division_rate = 1000000; // one million
-    
     *px_reg = player_x;
     *py_reg = player_y;
-    //the vlaues in button_reg are received from the physical button
+    //initial bullet coordinates  
+    uint32_t bullet_x;
+    uint32_t bullet_y = 500;  // start off screen
+    uint8_t bullet_active = 0;
+    *bullet_py_reg = 500;     // initialize off screen
+    //counter
+    uint32_t player_counter = 0; 
+    uint32_t bullet_counter = 0; 
+    const uint32_t player_division_rate = 300000;
+    const uint32_t bullet_division_rate = 50000;
+
     while (1) {
-        //bit masks used so when we add shooting, we can move and shoot at the same time
-          //If we added == instead of &, that would evaluate the entire value
-        if (counter >= division_rate) {
+        /*             player logic            */
+        if (player_counter >= player_division_rate) {
             if (*btn_reg & BTN_LEFT) {
-                //move left 1 pixel
-                //if (player_x >= 0) {
-                    *px_reg = --player_x;
-                //}
+                *px_reg = --player_x;
             } 
             if (*btn_reg & BTN_RIGHT) {
-                //move right 1 pixel
-                //if (player_x <= 639) { temporary removal
-                    *px_reg = ++player_x;
-                    //After we surpass 639, we can no longer move
-                //}
+                *px_reg = ++player_x;
             }
-            counter = 0;
+            if ((*btn_reg & BTN_FIRE) && !bullet_active) {
+                bullet_x = player_x;
+                bullet_y = player_y;
+                *bullet_px_reg = bullet_x;
+                *bullet_py_reg = bullet_y;
+                bullet_active = 1;
+            }
+            player_counter = 0;
         } else {
-            counter++;
+            player_counter++;
         }
+
+        //bullet logic 
+        if (bullet_counter >= bullet_division_rate) {
+            if (bullet_active) {
+                if (bullet_y == 0) {
+                    bullet_active = 0;
+                    *bullet_py_reg = 500;  // move off screen
+                } else {
+                    *bullet_py_reg = --bullet_y;
+                }
+            }
+            bullet_counter = 0;
+        } else {
+            bullet_counter++;
+        }
+
+        /*              alien logic              */
+
+
+
     }
     return (0);
 }
